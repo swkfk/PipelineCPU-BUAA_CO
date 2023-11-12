ba31b1e12c8ebef973184e2c51fe48a3a9d9e538
kai_Ker <kai_Ker@buaa.edu.cn>
2023-11-12 15:03:11 +0800

    [fix] Fix the omissive forward for PC8$E

--- mips.v
+++ mips.v
@@ -125,11 +125,13 @@ module mips(
     wire [4:0]  RegRA1, RegRA2, RegWA;
     
     /*** vvv Forward: D Stage vvv ***/
-    assign RegRD1$FWD = (RegRA1 == A3$M && A3$M && RegWriteEn$M && RegWriteSrc$M == `REGWr_Alu) ? AO$M :
+    assign RegRD1$FWD = (RegRA1 == A3$E && A3$E && RegWriteEn$E && RegWriteSrc$E == `REGWr_PC4) ? PC8$E :
+                        (RegRA1 == A3$M && A3$M && RegWriteEn$M && RegWriteSrc$M == `REGWr_Alu) ? AO$M :
                         (RegRA1 == A3$M && A3$M && RegWriteEn$M && RegWriteSrc$M == `REGWr_PC4) ? PC8$M :
                         (RegRA1 == A3$W && A3$W && RegWriteEn$W) ? WD$_W :
                         RegRD1;
-    assign RegRD2$FWD = (RegRA2 == A3$M && A3$M && RegWriteEn$M && RegWriteSrc$M == `REGWr_Alu) ? AO$M :
+    assign RegRD2$FWD = (RegRA2 == A3$E && A3$E && RegWriteEn$E && RegWriteSrc$E == `REGWr_PC4) ? PC8$E :
+                        (RegRA2 == A3$M && A3$M && RegWriteEn$M && RegWriteSrc$M == `REGWr_Alu) ? AO$M :
                         (RegRA2 == A3$M && A3$M && RegWriteEn$M && RegWriteSrc$M == `REGWr_PC4) ? PC8$M :
                         (RegRA2 == A3$W && A3$W && RegWriteEn$W) ? WD$_W :
                         RegRD2;
