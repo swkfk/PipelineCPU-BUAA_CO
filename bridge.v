89fd245b0dabae8920ccf248f917eb0e0eede4c1
kai_Ker <kai_Ker@buaa.edu.cn>
2023-11-26 12:28:33 +0800

    [fix] Fix the wrong punctuation

--- bridge.v
+++ bridge.v
@@ -4,29 +4,29 @@
 
 module Bridge(
     // For Processor
-    input  [31: 0] p_addr;
-    input  [ 3: 0] p_byteen;
-    input  [31: 0] p_wdata;
-    output [31: 0] p_rdata;
-    input  [31: 0] p_pc;
+    input  [31: 0] p_addr,
+    input  [ 3: 0] p_byteen,
+    input  [31: 0] p_wdata,
+    output [31: 0] p_rdata,
+    input  [31: 0] p_pc,
     // For DM
-    output [31: 0] m_addr;
-    input  [31: 0] m_rdata;
-    output [31: 0] m_wdata;
-    output [31: 0] m_byteen;
-    output [31: 0] m_pc;
+    output [31: 0] m_addr,
+    input  [31: 0] m_rdata,
+    output [31: 0] m_wdata,
+    output [31: 0] m_byteen,
+    output [31: 0] m_pc,
     // For TC0 & TC1
-    output [31: 2] tc0_addr;
-    output         tc0_we;
-    output [31: 0] tc0_wdata;
-    input  [31: 0] tc0_rdata;
-    output [31: 2] tc1_addr;
-    output         tc1_we;
-    output [31: 0] tc1_wdata;
-    input  [31: 0] tc1_rdata;
+    output [31: 2] tc0_addr,
+    output         tc0_we,
+    output [31: 0] tc0_wdata,
+    input  [31: 0] tc0_rdata,
+    output [31: 2] tc1_addr,
+    output         tc1_we,
+    output [31: 0] tc1_wdata,
+    input  [31: 0] tc1_rdata,
     // For Interrupt
-    output [31: 0] int_addr;
-    output [ 3: 0] int_byteen;
+    output [31: 0] int_addr,
+    output [ 3: 0] int_byteen
 //    input [31:0] VAddr,
 //    input [2:0]  AccessType,
 //    input [31:0] WriteData,
@@ -67,9 +67,9 @@ module Bridge(
     assign int_byteen = _dm ? p_byteen : 4'b0000;
     
     // Read Data from outers to the processor
-    assign p_rdata = _dm  ? m_rdata :
-                     _tc0 ? tc0_rdata:
-                     _tc1 ? tc1 _ rdata:
+    assign p_rdata = _dm  ? m_rdata   :
+                     _tc0 ? tc0_rdata :
+                     _tc1 ? tc1_rdata :
                      32'h0000_0000;
 
 
